b5497bb1a248ca970c9cd0354594c04b5c6c6825
svc-student <svc-student@student.directory.jccc>
2016-09-22 20:17:55 -0500

    Initial

---
+++ PA2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+
+	int playerHP[4];
+	bool done = false;
+
+	while (!done)
+	{
+
+	}
+
+	return 0;
+}
