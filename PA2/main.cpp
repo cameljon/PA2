a7b456a520487ddf9880fe2cdb92e3fd50d459a2
Jon <volden.jon@gmail.com>
2016-09-23 16:04:10 -0500

    Changes

--- PA2/main.cpp
+++ PA2/main.cpp
@@ -7,13 +7,83 @@ using namespace std;
 int main()
 {
 	srand(time(NULL));
-
-	int playerHP[4];
+	int playerHP[4] = {100, 100, 100, 100};
+	int round = 1;
+	bool notSuicide;
 	bool done = false;
+    int attackWho;
 
 	while (!done)
 	{
+	    //Menu for each round
+	    cout << endl << "Round " << round << ": FIGHT!" << endl << endl;
+	    for (int i = 0; i < 4; i++)
+        {
+            //Extra: HP bar
+            string hpBar;
+            for (int h = 0; h < playerHP[i]; h += 2)
+            {
+                hpBar += "#";
+            }
+            string npcOrYou;
+            if (i == 0)
+            {
+                npcOrYou = "(You!)";
+            }
+            else
+            {
+                npcOrYou = "(NPC) ";
+            }
+            cout << "Player " << i << " " << npcOrYou << " HP: " << playerHP[i] << "\t" << hpBar << endl;
+        }
 
+        notSuicide = false;
+        while (!notSuicide)
+        {
+            cout << endl << "Who do you want to attack? ";
+            cin >> attackWho;
+            if (attackWho != 0)
+            {
+                playerHP[attackWho] -= 5;
+                cout << endl << "You attack player " << attackWho << " for 5 damage." << endl;
+                notSuicide = true;
+            }
+            else
+            {
+                cout << "You can't attack yourself. Pick again!" << endl;
+            }
+
+        }
+
+        //NPC's turn
+        for (int i = 1; i < 4; i++)
+        {
+            //Extra: Don't attack self? Theoretical infinite loop?
+            notSuicide = false;
+            while (!notSuicide)
+            {
+                attackWho = rand() % 4;
+                if (i != attackWho)
+                {
+                    notSuicide = true;
+                }
+            }
+
+            playerHP[attackWho] -= 5;
+            cout << "Player " << i << " attacks player " << attackWho << " for 5 damage!" << endl;
+        }
+
+        //Check if someone is dead
+        for (int i = 0; i < 4; i++)
+        {
+            if (playerHP[i] <= 0)
+            {
+                cout << "Player " << i << " is dead!" << endl;
+                done = true;
+            }
+        }
+        round++;
+        cout << endl << "---------------------------------------------------------------------" << endl;
 	}
 
 	return 0;
--- README.md
+++
@@ -1,1 +0,0 @@
-Due Oct 6th
